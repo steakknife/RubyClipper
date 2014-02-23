31ec6b8ffa50f1e92196c2b930341b1d4367cb53
Barry Allard <barry.allard@gmail.com>
2014-02-23 05:29:08 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyClipper::Application.config.secret_key_base = '61802dfd3d2925577d38ed100d3b62487ec94353328745aacafeb12a48aaa0c501eca11163599ab4765c7a10153987544f00726940a826149a80ce5d26a3e175'
